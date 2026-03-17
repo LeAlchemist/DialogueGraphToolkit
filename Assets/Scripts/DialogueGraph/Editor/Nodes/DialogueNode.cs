b1b96d96b0d39bcb0cbce2417abf86550fe264e5
Nicholas Ross <RossNicholasV@gmail.com>
2026-03-17 10:30:49 -0500

    Added Dialogue Node

---
+++ Assets/Scripts/DialogueGraph/Editor/Nodes/DialogueNode.cs
@@ -0,0 +1,31 @@
+using System;
+using Unity.GraphToolkit.Editor;
+using UnityEngine;
+
+[Serializable]
+class DialogueNode : Node
+{
+    public const string InPortName = "In";
+    public const string OutPortName = "Out";
+    public const string SpeakerPortName = "Speaker";
+    public const string DialoguePortName = "Dialogue";
+
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddInputPort(InPortName)
+        .WithDisplayName(string.Empty)
+        .WithConnectorUI(PortConnectorUI.Arrowhead)
+        .Build();
+
+        context.AddInputPort<string>(SpeakerPortName)
+        .Build();
+
+        context.AddInputPort<string>(DialoguePortName)
+        .Build();
+
+        context.AddOutputPort(OutPortName)
+        .WithDisplayName(string.Empty)
+        .WithConnectorUI(PortConnectorUI.Arrowhead)
+        .Build();
+    }
+}
---
+++ Assets/Scripts/DialogueGraph/Runtime/Nodes/RuntimeDialogueNode.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class RuntimeDialogueNode
+{
+    public string NodeID;
+    public string SpeakerName;
+    public string DialogueText;
+    public string NextNodeID;
+}
